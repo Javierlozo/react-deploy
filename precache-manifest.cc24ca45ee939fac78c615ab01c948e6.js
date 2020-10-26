cb3b5d8ffe1f93c8e376804e689216b17dbfc6ef
Luis Javier Lozoya <luisloart@gmail.com>
2020-10-26 10:11:53 -0400

    Updates

---
+++ precache-manifest.cc24ca45ee939fac78c615ab01c948e6.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "ab61766c728c4a21746ec7edfb25a93b",
+    "url": "/react-deploy/index.html"
+  },
+  {
+    "revision": "635b737f0e91d9afcf86",
+    "url": "/react-deploy/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fb1861247f5cca0e66a7",
+    "url": "/react-deploy/static/js/2.59f09529.chunk.js"
+  },
+  {
+    "revision": "570d362d673dab785e62d2b8563e1118",
+    "url": "/react-deploy/static/js/2.59f09529.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "635b737f0e91d9afcf86",
+    "url": "/react-deploy/static/js/main.c89763f5.chunk.js"
+  },
+  {
+    "revision": "36d95b0805722cb5c6e1",
+    "url": "/react-deploy/static/js/runtime-main.05342921.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/react-deploy/static/media/logo.5d5d9eef.svg"
+  }
+]);
